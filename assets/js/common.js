6fcf75c6f1aae4b5e0498c9ec6d65e7b25c81760
Steve <anonymouse@bestaatniet.com>
2020-04-23 17:15:44 +0200

    javascript werkt, nu nog op link blijven

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,1 +1,16 @@
-$('.navbar-js').load('/assets/common/navbar.html');
+$('.navbar-js').load('/assets/common/navbar.html')
+
+
+let links = document.getElementsByClassName("js-vaklink")
+Array.from(links).forEach(function (link) {
+    console.log(link)
+    link.addEventListener('click', function () {
+        if (window.innerWidth < 1200) {
+            let content = link.nextElementSibling
+            let child = link.firstElementChild
+
+            content.classList.toggle("gridFront__content--click")
+            child.classList.toggle(child.classList[0] + "--click")
+        }
+    })
+})
