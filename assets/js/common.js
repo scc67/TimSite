1625392e0788846b067914543ee062c08c7559a2
Steve <anonymouse@bestaatniet.com>
2020-04-23 21:14:38 +0200

    aanpassing openklappen bij klikken en centraal in beeld komen.

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,12 +5,27 @@ let links = document.getElementsByClassName("js-vaklink")
 Array.from(links).forEach(function (link) {
     console.log(link)
     link.addEventListener('click', function () {
+        let content = link.nextElementSibling
+        let child = link.firstElementChild
         if (window.innerWidth < 1200) {
-            let content = link.nextElementSibling
-            let child = link.firstElementChild
-
-            content.classList.toggle("gridFront__content--click")
-            child.classList.toggle(child.classList[0] + "--click")
+            if (content.classList.contains("gridFront__content--click")) {
+                content.classList.remove("gridFront__content--click")
+                child.classList.remove(child.classList[0] + "--click")
+            } else {
+                resetlinks()
+                content.classList.add("gridFront__content--click")
+                child.classList.add(child.classList[0] + "--click")
+            }
         }
     })
 })
+
+function resetlinks() {
+    Array.from(links).forEach(function (link) {
+        let content = link.nextElementSibling
+        let child = link.firstElementChild
+
+        content.classList.remove("gridFront__content--click")
+        child.classList.remove(child.classList[0] + "--click")
+    })
+}
