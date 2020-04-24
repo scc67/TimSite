d104ee82c8ceb62c0c13acab5eb41bc24afc6e8d
Steve <anonymouse@bestaatniet.com>
2020-04-24 19:42:17 +0200

    java syntacs kloppend gemaakt door jslint

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,29 +1,29 @@
-$('.navbar-js').load('/assets/common/navbar.html')
+$('.navbar-js').load('/assets/common/navbar.html');
 
 
-let links = document.getElementsByClassName("js-vaklink")
+let links = document.getElementsByClassName("js-vaklink");
 Array.from(links).forEach(function (link) {
-    console.log(link)
+    console.log(link);
     link.addEventListener('click', function () {
-        let content = link.nextElementSibling
-        let child = link.firstElementChild
+        let content = link.nextElementSibling;
+        let child = link.firstElementChild;
         if (content.classList.contains("gridFront__content--click")) {
-            content.classList.remove("gridFront__content--click")
-            child.classList.remove(child.classList[0] + "--click")
+            content.classList.remove("gridFront__content--click");
+            child.classList.remove(child.classList[0] + "--click");
         } else {
-            resetlinks()
-            content.classList.add("gridFront__content--click")
-            child.classList.add(child.classList[0] + "--click")
+            resetlinks();
+            content.classList.add("gridFront__content--click");
+            child.classList.add(child.classList[0] + "--click");
         }
-    })
-})
+    });
+});
 
 function resetlinks() {
     Array.from(links).forEach(function (link) {
-        let content = link.nextElementSibling
-        let child = link.firstElementChild
+        let content = link.nextElementSibling;
+        let child = link.firstElementChild;
 
-        content.classList.remove("gridFront__content--click")
-        child.classList.remove(child.classList[0] + "--click")
-    })
+        content.classList.remove("gridFront__content--click");
+        child.classList.remove(child.classList[0] + "--click");
+    });
 }
